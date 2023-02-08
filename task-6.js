6b375a2b4f29ea51c2ac7bbb6d6502d75233cb24
Ilgiz Ayupov <ilgizayupov7@gmail.com>
2023-02-08 16:38:44 +0500

    Start: Task 6

---
+++ task-6.js
@@ -0,0 +1,11 @@
+const buildMultiplicationTable = (limit) => {
+  const table = "";
+
+  for (let row = 0; row < limit; row++) {
+    table += `${row}\n`
+  }
+};
+
+console.log(
+    buildMultiplicationTable(5)
+)
