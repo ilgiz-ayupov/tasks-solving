c82face795f4ec78b305b95df636f6816e584bfc
Ilgiz Ayupov <ilgizayupov7@gmail.com>
2023-02-08 16:51:58 +0500

    Finish: Task 6

--- task-6.js
+++ task-6.js
@@ -1,11 +1,18 @@
-const buildMultiplicationTable = (limit) => {
-  const table = "";
+const multiplicationTable = (limit) => {
+  const showRow = (row, line) => console.log(row || " ", line.join(" "));
 
-  for (let row = 0; row < limit; row++) {
-    table += `${row}\n`
+  for (let row = 0; row <= limit; row++) {
+    let line = [];
+    for (let number = 1; number <= limit; number++) {
+      if (row === 0) {
+        line.push(number);
+      } else {
+        line.push(number * row);
+      }
+    }
+
+    showRow(row, line);
   }
 };
 
-console.log(
-    buildMultiplicationTable(5)
-)
+multiplicationTable(9);
